bdacf5fb9ec69a94035e0504db8be484b816622e
Duc <duc8anv2@gmail.com>
2022-05-25 17:02:51 +0700

    use localstorage to save things

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-function togglePopup() {
+const NoteList = document.querySelector(".lists");
+
+var dataString = localStorage.getItem("New Note");
+var note = JSON.parse(dataString); 
+
+const note_title = document.getElementById("note-title");
+const note_text = document.getElementById("note-text");
+const note_time = document.getElementById("note-time");
+
+function turnPage() {
     window.location.href="text.html";
 }
 
+function togglePopup(index) {
+    document.getElementById("popup-1").classList.toggle("active");
+    document.getElementById("overlay").classList.toggle("active");
+    note_title.innerHTML = note[index].title;
+    note_text.innerHTML = note[index].description;
+    note_time.innerHTML = note[index].time;
+}
+
+showTasks();
+
+function showTasks(){
+    let getLocalStorageData = localStorage.getItem("New Note");
+    if(getLocalStorageData == null){
+        note = [];
+    }   
+    let newLiTag = "";
+    for(let index=0; index<note.length; index++) {
+      let element = note[index].title;
+      newLiTag +=   `<li class="note-list"><p onclick="togglePopup(${index})">${element}</p><div class="icon"><button><i class="fa-solid fa-bookmark"></i></button><button><i class="fa-solid fa-pen-to-square"></i></button> <button><i class="fa-solid fa-circle-down"></i></button><button onclick="deleteTask(${index})"><i class="fa-solid fa-trash"></i></button></div></li>`
+    };
+    
+    NoteList.innerHTML = newLiTag; //adding new li tag inside ul tag
+    inputBox.value = ""; //once task added leave the input field blank
+}
+// delete task function
+function deleteTask(index){
+    note.splice(index, 1); //delete or remove the li
+    localStorage.setItem("New Note", JSON.stringify(note));
+    showTasks(); //call the showTasks function
+}
--- text_script.js
+++ text_script.js
@@ -1,3 +1,35 @@
 function returnPage() {
     window.location.href="index.html";
 }
+
+const inputBox = document.querySelector(".header input");
+const addBtn = document.querySelector(".add-btn");
+
+var dataString = localStorage.getItem("New Note");
+var note = JSON.parse(dataString); 
+
+var d = new Date();// thoi gian hien tai
+var ngay = d.getDate();
+var thang = d.getMonth() + 1;
+var nam = d.getFullYear();
+var gio = d.getHours();
+var phut = d.getMinutes();
+var giay = d.getSeconds();
+var sngay = ngay + '/' + thang + '/' + nam + " " + gio + ":" + phut + ":" + giay;
+
+
+addBtn.onclick = ()=>{ //when user click on plus icon button
+    let userEnteredValue = inputBox.value; //getting input field value
+
+    let getLocalStorageData = localStorage.getItem("New Note"); //getting localstorage
+    if(getLocalStorageData == null){ //if localstorage has no data
+        note = []; //create a blank array
+    }
+    note.push({
+      title:userEnteredValue,
+      description:tinymce.get("myTextarea").getContent(),
+      time:sngay
+    }); //pushing or adding new value in array
+    localStorage.setItem("New Note", JSON.stringify(note)); //transforming js object into a json string
+    returnPage();
+}
