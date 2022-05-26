7dac83749adb8dc4dab10f31b4d24d99bca1ceca
Duc <duc8anv2@gmail.com>
2022-05-26 16:03:17 +0700

    add save function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var note = JSON.parse(dataString);
 const note_title = document.getElementById("note-title");
 const note_text = document.getElementById("note-text");
 const note_time = document.getElementById("note-time");
+const download_content = document.querySelector(".popup");
 
 function turnPage() {
     window.location.href="text.html";
@@ -29,7 +30,7 @@ function showTasks(){
     let newLiTag = "";
     for(let index=0; index<note.length; index++) {
       let element = note[index].title;
-      newLiTag +=   `<li class="note-list"><p onclick="togglePopup(${index})">${element}</p><div class="icon"><button><i class="fa-solid fa-bookmark"></i></button><button><i class="fa-solid fa-pen-to-square"></i></button> <button><i class="fa-solid fa-circle-down"></i></button><button onclick="deleteTask(${index})"><i class="fa-solid fa-trash"></i></button></div></li>`
+      newLiTag +=   `<li class="note-list"><p>${element}</p><div class="icon"><button onclick="togglePopup(${index})"><i class="fa-solid fa-eye"></i></button><button onclick="Edit(${index})"><i class="fa-solid fa-pen-to-square"></i></button> <button onclick="SaveFile(${index})"><i class="fa-solid fa-circle-down"></i></button><button onclick="deleteTask(${index})"><i class="fa-solid fa-trash"></i></button></div></li>`
     };
     
     NoteList.innerHTML = newLiTag; //adding new li tag inside ul tag
@@ -40,4 +41,19 @@ function deleteTask(index){
     note.splice(index, 1); //delete or remove the li
     localStorage.setItem("New Note", JSON.stringify(note));
     showTasks(); //call the showTasks function
+}
+
+function Edit(index) {
+    window.location.href="text.html";
+}
+
+function SaveFile(index) {
+    var name = note[index].title;
+    var content = note[index].description;
+    var myFile = new File(
+        [content],
+        name + ".txt",
+        {type: "text/plain;charset:utf8"}
+    );
+    saveAs(myFile);
 }
